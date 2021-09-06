85cab79b7ee7481c496f586e9fcdb7b0df3c0a96
Adrian Castellanos <adriancg@gmail.com>
2021-09-05 20:20:41 -0500

    Migrate from Tabletop to Papaparse

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,5 +1,5 @@
 import { LightningElement } from 'lwc';
-import Tabletop from 'tabletop';
+import Papa from 'papaparse';
 
 const columns = [
     { label: 'Nombre', fieldName: 'nombre', wrapText: true },
@@ -12,13 +12,15 @@ export default class App extends LightningElement {
     loading = true;
 
     connectedCallback() {
-        Tabletop.init({
-            key:
-                'https://docs.google.com/spreadsheets/d/1aLqw2lTVpYHAW1LPtnrQ9w3cgSXDHFP_Ot1CDfEX9zM/edit?usp=sharing',
-            simpleSheet: true
-        }).then((data) => {
-            this.data = data.filter((row) => row.telefono);
-            this.loading = false;
+        let sheetId = '1aLqw2lTVpYHAW1LPtnrQ9w3cgSXDHFP_Ot1CDfEX9zM';
+        let sheetUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/pub?output=csv`;
+        Papa.parse(sheetUrl, {
+            download: true,
+            header: true,
+            complete: ({ data }) => {
+                this.data = data.filter((row) => row.telefono);
+                this.loading = false;
+            }
         });
     }
 }
